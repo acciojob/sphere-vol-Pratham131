42119c9c61e86144e9b4771994dade8307626496
Pratham131 <104488785+Pratham131@users.noreply.github.com>
2023-07-20 09:23:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,16 @@ function volume_sphere() {
     //Write your code here
 	const radius = document.getElementById("radius").value;
 
-	// Check if the radius is a valid number
     if (isNaN(radius)) {
         alert("Please enter a valid number for the radius.");
         return;
     }
 	
-	let volume = (4/3) * Math.PI * Math.pow(radius, 3);
+	let volumeOfSphere = (4/3) * Math.PI * Math.pow(radius, 3);
 
-	document.getElementById("volume").value = volume.toFixed(2);
-
-	// var radius = parseFloat(document.getElementById("radius").value);
-
-    
+	document.getElementById("volume").value = volumeOfSphere
+		// .toFixed(2);
+   
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
